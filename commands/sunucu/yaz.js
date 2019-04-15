8750f5283f73c581b703e2678d9e36f8ca0e35e6
BloodyX <21262005+turkhd@users.noreply.github.com>
2019-04-15 13:15:56 +0300

    Add files via upload

---
+++ commands/sunucu/yaz.js
@@ -0,0 +1,33 @@
+const { Command } = require('discord.js-commando');
+const { RichEmbed } = require('discord.js');
+
+module.exports = class YazCommand extends Command {
+    constructor(client){
+
+        super(client, {
+
+            name: 'yaz',
+            group: 'sunucu',
+            memberName: 'yaz',
+            description: 'Bota yazı yazdırırsınız.',
+
+            args: [
+                {
+                    key: 'yazilan',
+                    prompt: 'Bota ne yazdırmak istersin?',
+                    type: 'string'
+                }
+            ]
+        })
+    }
+
+
+    run(msg, args) {
+    const { yazilan } = args;
+    const embed = new RichEmbed()
+    .setColor('RANDOM')
+    .setDescription(yazilan)
+    return msg.embed(embed)
+
+    }
+}
---
+++ server.js
@@ -0,0 +1,50 @@
+const { CommandoClient, SQLiteProvider } = require('discord.js-commando');
+const path = require('path'),
+moment = require('moment'),
+sqlite = require('sqlite');
+
+const ayarlar = require('./data/ayarlar.json');
+
+const client = new CommandoClient({
+    commandPrefix: ayarlar.PREFIX,
+    unknownCommandResponse: false,
+    owner: ayarlar.SAHIP,
+    disableEveryone: false
+});
+
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+		['sunucu', 'Sunucu Komutları'],
+		['bot', 'Bot Komutları'],
+		['ayarlar', 'Ayarlar'],
+		['admin', 'Admin'],
+        ['moderasyon', 'Moderasyon'],
+        ['başvuru', 'Başvuru'],
+        ['kullanıcı', 'Kullanıcı'],
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
+	sqlite.open(path.join(__dirname, "json.sqlite")).then((db) => {
+		client.setProvider(new SQLiteProvider(db));
+	});
+
+client.on('ready', () => {
+client.user.setPresence({
+        game: {
+            name: 'Henüz Yardım Komutu Yok Ama Prefixim "."',
+            type: "STREAMING",
+            url: "https://www.twitch.tv/test"
+        }
+    });
+
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] LOG: Aktif, Komutlar yüklendi!`);
+});
+
+client.on('error', err => {
+	console.log(err)
+});
+
+client.login(ayarlar.TOKEN);
